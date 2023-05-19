fb87c4b66d4b26f62cc25b2843a869c2d084ff62
Joyce Wambui Nyambura <124131085+wambui-nyambura-joyce@users.noreply.github.com>
2023-05-19 19:15:48 +0300

    Updated product class to show the items available in stock

--- product.py
+++ product.py
@@ -1,26 +1,4 @@
-class ItemsAvailable:
-    def __init__(self,item_name,price,addtocart, available_groceries) :
-        self.item_name=item_name
-        self.price=price
-        self.addtocart=addtocart
-        self.groceries = available_groceries
-    def add_item(self):
-        self.add=self.addtocart
-#Checking if the item selection is available in stock
-        if self.item_name in self.groceries and self.price in self.groceries:
-            self.item_name==True
-            return(f"{self.item_name} added to basket")
-        else:
-            self.item_name == False
-            return(f"{self.item_name} is not available")
-        
-    print(add_item("mango"))
-    
-# returning the final state of the button "add to cart"
-        # return self.add
-# use example
-# item = ItemsAvailable("pineapple", 200, True, {"pineapple": 180, "mango": 100})
-# print(item.add_item(True))
+
 class Product:
     def __init__(self, name, price, stock):
         self.name = name
@@ -48,7 +26,7 @@ class Product:
         if self.is_available():
             if quantity <= self.stock:
                 self.update_stock(-quantity)
-                return f"Purchased {quantity} {self.name}(s)"
+                return f"Purchased {quantity} {self.name}"
             else:
                 return f"Insufficient stock for {self.name}"
         else:
