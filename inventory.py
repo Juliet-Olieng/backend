7eb695cbe1b161f3fab4f5351a3bf5883d4989f4
Serah-Wanjiru <124131607+Serah-Wanjiru@users.noreply.github.com>
2023-05-19 19:20:24 +0300

    Updated inventory class to show the quantity purchased.

--- inventory.py
+++ inventory.py
@@ -1,21 +1,23 @@
 class InventoryItem:
     def __init__(self, product_name, product_price, product_quantity):
-        self.name=product_name
-        self.price= product_price 
-        self.quantity = product_quantity
+        self.product_name=product_name
+        self.product_price= product_price 
+        self.product_quantity = product_quantity
     def add_inventory(self):
         inventory_dict={
-           "name":self.product_name,
-           "order":self.product_price,
-           "payment":self.product_quantity,
+           "product_name":self.product_name,
+           "product_price":self.product_price,
+           "product_quantity":self.product_quantity,
        
        }
         return inventory_dict
-product_name=input("input your name: ")
-product_price=input("Input the type of order: ")
-product_quantity=input("Input the type of payment: ")
+product_name=input("input product name: ")
+product_price=input("Input the price of the product: ")
+product_quantity=input("Input the quantity of the products: ")
 
 new_Customer=InventoryItem(product_name,product_price,product_quantity)
 user=new_Customer
 print(new_Customer.add_inventory())
+
+
 
