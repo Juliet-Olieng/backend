9e71980a9ff598635cafda270e6f671963a46f0a
Serah-Wanjiru <124131607+Serah-Wanjiru@users.noreply.github.com>
2023-05-19 19:25:52 +0300

    Updated add_item.py to show the quantity of items

--- add_item.py
+++ add_item.py
@@ -1,23 +1,23 @@
-class InventoryItem:
-    def __init__(self, name, price, quantity):
-        self.name=name
-        self.price=price
-        self.quantity =quantity
+class add_item:
+    def __init__(self, item_name, item_price, item_quantity):
+        self.item_name=item_name
+        self.item_price=item_price
+        self.item_quantity =item_quantity
         
     def getItem(self):
           user_item= {
-            "name": self.name,
-            "email": self.price,
-            "password": self.quantity,
+            "iten_name": self.item_name,
+            "item_price": self.item_price,
+            "item_quantity": self.item_quantity,
            
         } 
           return user_item
-name=input("input your name: ")
-price=input("Input the type of price: ")
-quantity=input("Input the quantity: ")
+item_name=input("input the product_name : ")
+item_price=input("Input the  price of the products: ")
+item_quantity=input("Input the quantity of the products: ")
 
     # An instance of order is assigned to a variable called user
-new_Customer=InventoryItem(name,price,quantity)
+new_Customer=add_item(item_name,item_price,item_quantity)
 user=new_Customer
 print(new_Customer.getItem())
         
